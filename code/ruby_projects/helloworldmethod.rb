7be3f9c0e4d94e243bbd21b01f80bcd2c222f027
Sarah Maria Leidisch <shivelle@gmx.net>
2017-10-13 15:07:07 +0000

    working with strings

---
+++ code/ruby_projects/helloworldmethod.rb
@@ -0,0 +1,6 @@
+def hello(anything)
+   puts anything
+end
+
+hello "My name is Sarah!"
+hello "Check it out. This prints anything I want."
---
+++ code/ruby_projects/helloworldstring.rb
@@ -0,0 +1,4 @@
+hello = "Hello World!"
+puts hello
+hello = "Reassigned the Variable!"
+puts hello
