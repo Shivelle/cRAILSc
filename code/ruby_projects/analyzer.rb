9c4b4bb32c11ea640514a309ebff4a3ace403871
Sarah Maria Leidisch <shivelle@gmx.net>
2017-10-19 09:22:16 +0000

    working with numbers

--- code/ruby_projects/analyzer.rb
+++ code/ruby_projects/analyzer.rb
@@ -15,3 +15,19 @@ name = first_name + " " + last_name
 puts "Hello #{name}, welcome to the analyzer!"
 puts "Your first name has #{first_name.length} characters. Your last name has #{last_name.length} characters."
 puts "Your full name reversed reads: #{name.reverse}"
+
+puts "Please enter your first number"
+first_number = gets.chomp
+puts "Please enter your second number"
+second_number = gets.chomp
+puts "The first number multiplied by the second number is:  #{first_number.to_i * second_number.to_i} "
+
+
+# todo 
+# the first number divided by the second number is
+# the first number substracted from the second number is
+# the first number mod of the second number is
+
+puts "The first number divided by the second number is:  #{first_number.to_i / second_number.to_i} "
+puts "The first number substracted by the second number is:  #{second_number.to_i - first_number.to_i} "
+puts "The first number mod of the second number is:  #{first_number.to_i % second_number.to_i} "
